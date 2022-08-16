6090659ec7974dd7d8c8281f0363153983dae911
liimmaa <halimammegwa@gmail.com>
2022-08-16 16:08:05 +0100

    Initial Commit

---
+++ RyanAirApp/RyanAirAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RyanAirAppApp.swift
+//  RyanAirApp
+//
+//  Created by Chioma Amanda Mmegwa  on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RyanAirAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
